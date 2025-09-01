63b13ce2a9140bd2eaae565a5eb5b8a2ec715c42
ArijAguel <arij.aguel@insat.ucar.tn>
2025-09-02 00:06:40 +0100

    fixed front

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://test.siratify.com/api/v1',
-  wsUrl: 'wss://test.siratify.com/ws',
-  siteKey: '6Ld65EsqAAAAAIgn1tPAEHoX8Z3ieXlckgrPqPes'
-
-
-};
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-
-  production: true,
-
-  apiUrl: 'https://siratify.com/api/v1',
-  wsUrl: 'wss://siratify.com/ws',
-  siteKey: '6Ld65EsqAAAAAIgn1tPAEHoX8Z3ieXlckgrPqPes'
-
-};
